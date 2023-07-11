2b037af97a237b6fd203047a94a8268a3a519b3f
Lrx <209714995@qq.com>
2023-07-11 21:54:15 +0800

    master commit 2

--- src/main/java/com/atguigu/App.java
+++ src/main/java/com/atguigu/App.java
@@ -14,5 +14,6 @@ public class App
         System.out.println("hello 4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
